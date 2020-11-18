bcf8238cc276159b8bda24b962277449bdf08299
Ashley <ashchat2008@gmail.com>
2020-11-18 13:07:51 -0500

    Update Javasript file and added image folder

--- Portfolio1/script.js
+++ Portfolio1/script.js
@@ -3,6 +3,11 @@ console.log(tool);
 
 var d = new Date();
 var months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct","Nov","Dec"]; //Array for each month
-document.getElementById("day").innerHTML = d.getDate();
-document.getElementById("month").innerHTML = months[d.getMonth()];    //Get the current date base on the Array
-document.getElementById("year").innerHTML = d.getFullYear();
+
+var d = new Date();
+var curr_date = d.getDate();
+var curr_month = months[d.getMonth()];
+var curr_year = d.getFullYear();
+var dateStr = curr_date + "-" + curr_month + "-" + curr_year;
+document.getElementById("test").innerHTML = dateStr;
+//console.log(curr_date + "-" + curr_month + "-" + curr_year);
